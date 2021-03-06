a54fcb859e18db92d079ac64fedc29f83b355626
Emil <emilfigzal@gmail.com>
2021-03-06 17:27:49 +0100

    configuration metadata using annotation processor

--- src/main/java/pl/emil7f/simplerestapi/SimpleRestApiApplication.java
+++ src/main/java/pl/emil7f/simplerestapi/SimpleRestApiApplication.java
@@ -2,12 +2,23 @@ package pl.emil7f.simplerestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleRestApiApplication {
+
+    private UserProperties userProperties;
+
+    public SimpleRestApiApplication(UserProperties userProperties) {
+        this.userProperties = userProperties;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleRestApiApplication.class, args);
     }
 
+    @Bean
+    public void showMyUserProperties(){
+        System.out.println(userProperties.getLogin() + " " + userProperties.getPassword());
+    }
 }
---
+++ src/main/java/pl/emil7f/simplerestapi/UserProperties.java
@@ -0,0 +1,33 @@
+package pl.emil7f.simplerestapi;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+
+@ConfigurationProperties(prefix = "user")
+@Component
+@Validated
+public class UserProperties {
+
+    @NotNull
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
