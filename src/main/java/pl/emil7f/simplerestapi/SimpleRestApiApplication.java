c8dd90a8d0c1258630d3ca5f8de41739b0796c7c
Emil <emilfigzal@gmail.com>
2021-03-06 17:47:02 +0100

    configuration metadata directly

--- src/main/java/pl/emil7f/simplerestapi/SimpleRestApiApplication.java
+++ src/main/java/pl/emil7f/simplerestapi/SimpleRestApiApplication.java
@@ -1,34 +1,36 @@
 package pl.emil7f.simplerestapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 @SpringBootApplication
 public class SimpleRestApiApplication {
-
-
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleRestApiApplication.class, args);
     }
 
-    @Bean
-    public void showMyUserProperties(){
-        System.out.println(userProperties().getLogin() + " " + userProperties().getPassword());
-        System.out.println(databaseProperties().getLogin() + " " + databaseProperties().getPassword());
+    @Value("${my.information}")
+    private List<String> myInformation;
+
+    @Value("${show.myInformation}")
+    private boolean showInfo;
+
+    /*  Metoda wykona się tylko raz, zaraz po inicjalizacji     */
+    @PostConstruct
+    void showInfo() {
+        if (showInfo) {
+            myInformation.forEach(info -> System.out.println(info));
+        } else {
+            System.out.println("No information");
+        }
     }
 
-    @Bean
-    @ConfigurationProperties(prefix = "user")
-    MyLogProperties userProperties(){
-        return new MyLogProperties();
-    }
 
-    @Bean
-    @ConfigurationProperties(prefix = "database")
-    MyLogProperties databaseProperties(){
-        return new MyLogProperties();
-    }
 }
