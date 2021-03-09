5b6d7e0ec7488d736bd1defa565bc197ef10e18f
Emil <emilfigzal@gmail.com>
2021-03-09 18:53:14 +0100

    eureka configuration

--- src/main/java/pl/emil7f/simplerestapi/SimpleRestApiApplication.java
+++ src/main/java/pl/emil7f/simplerestapi/SimpleRestApiApplication.java
@@ -1,15 +1,11 @@
 package pl.emil7f.simplerestapi;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import javax.annotation.PostConstruct;
-import java.util.List;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SimpleRestApiApplication {
 
     public static void main(String[] args) {
